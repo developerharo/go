9b545e850fad10015dccbd81a454e3c603d8056b
developerharo <developerharo@gmail.com>
2021-09-23 19:04:30 -0500

    Seccion 4: Convenciones y sintaxis de GO. 7.- Variables en GO

---
+++ ejer02/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+var numero int
+var texto string
+var status bool = true
+
+func main() {
+	var numero2, numero3, numero4 int
+	numero2, numero3, numero4, texto, status = 2, 5, 67, "Este es mi texto", false
+
+	var moneda float32 = 0
+
+	numero2 = int(moneda)
+
+	texto = strconv.Itoa((int(moneda)))
+
+	fmt.Println(numero2)
+	fmt.Println(numero3)
+	fmt.Println(numero4)
+	fmt.Println(texto)
+	fmt.Println(status)
+
+	mostrarStatus()
+}
+
+func mostrarStatus() {
+	fmt.Println(status)
+}
